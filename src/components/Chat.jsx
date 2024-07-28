d0d914f80d9c9ae6cbedd7869c19e33fff15ee6d
yugyusang <yugyusang12@gmail.com>
2024-07-28 23:16:47 +0900

    채팅 기능 구현 및 css 작성중

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 import { CircleQueue } from '../utils/function/common';
+import '../styles/Chat.css';
+import { IoIosSend } from 'react-icons/io';
+import { useLocation } from 'react-router-dom';
 
 const Chat = () => {
     const SocketRef = useRef(null)
     const [isConnected, setIsConnected] = useState(false);
     const [message, setMessage] = useState('');
-    const messageQueue = useRef(new CircleQueue(5));
+    const messageQueue = useRef(new CircleQueue(10));
     const [messageList, setMessageList] = useState([]);
+    const liRefs = useRef([]);
+    const location = useLocation();
+
+    const queryParams = new URLSearchParams(location.search);
+    const searchParam = queryParams.get('search');
 
     useEffect(() => {
         // 서버 URL로 소켓 연결을 생성합니다.
-        SocketRef.current = io('http://localhost:3000', {
+        SocketRef.current = io('http://localhost:80', {
             reconnectionAttempts: 3, // 재연결 시도 횟수 설정
         });
 
-        // 연결 성공 이벤트
-        SocketRef.current.on('connect', () => {
-            console.log('서버와 연결되었습니다.');
-            SocketRef.current.emit('join', 'room1');
+        // 웹소켓 연결 성공시 룸 연결 이벤트
+        SocketRef.current && searchParam && SocketRef.current.on('connect', () => {
+            console.log('서버와 연결되었습니다 룸에 접속합니다.');
+            SocketRef.current.emit('join', searchParam);
             setIsConnected(true);
         });
 
@@ -39,47 +47,65 @@ const Chat = () => {
             setIsConnected(false);
         });
 
-        // 서버에 'message' 이벤트를 발생시킵니다.
-        // process.stdin.on('data', (data) => {
-        //     const input = data.toString().trim();
-        //     SocketRef.current.emit('message', input);
-        // });
-
         // 서버로부터 'message' 이벤트를 수신하면 콘솔에 메시지를 출력합니다.
         SocketRef.current.on('messageToClient', (data) => {
             console.log('서버로부터 메시지를 받았습니다:', data);
             messageQueue.current.enqueue(data);
-            setMessageList(messageQueue.current.list)
+            setMessageList(messageQueue.current.list);
         });
 
         return () => {
-            SocketRef.current.disconnect();
+            SocketRef.current.disconnect();// 컴포넌트가 언마운트 될 때 소켓 연결을 끊습니다.(자동으로 이벤트들도 제거됩니다.)
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+
+    useEffect(() => {
+        // 자동 연결이 안될때
+        SocketRef.current && searchParam && SocketRef.current.emit('join', searchParam);
+    }, [searchParam]);
 
     const changeMessagehandler = (e) => {
         setMessage(e.target.value);
     }
+
+    //서버로 메시지를 전송합니다.
     const sendMessageHandler = () => {
         if (message === '') {
             return;
         }
         if (SocketRef.current) {
             SocketRef.current.emit('message', message);
+            messageQueue.current.enqueue('당신:' + message);
+            setMessageList(messageQueue.current.list);
             setMessage('');
         }
     }
 
-    useEffect(() => { console.log(messageList) }, [messageList]);
+    useEffect(() => {
+        // 메시지가 많아지면 투명도를 조절합니다.(큐의 크기를 기준으로 함)
+        liRefs.current.forEach((item, index) => {
+            const visibleLength = messageQueue.current.size / 2;
+            if (liRefs.current.length > visibleLength && index < liRefs.current.length - visibleLength) {
+                item.style.opacity = 1 - (liRefs.current.length - visibleLength - index) / visibleLength;
+            }
+        })
+    }, [messageList]);
 
     return (
-        <div>
-            <input type='text' onChange={changeMessagehandler} value={message}>
-            </input>
-            <button onClick={sendMessageHandler}>전송</button>
+        <div className='chat_container'>
             <ul>
-                {messageList && messageList.map((item, index) => <li key={index}>{item}</li>)}
+                {messageList && messageList.map((item, index) =>
+                    item && <li key={index}
+                        ref={el => liRefs.current[index] = el}
+                    >{item}</li>)}
             </ul>
+            <div className='input_container'>
+                <input type='text' onChange={changeMessagehandler} value={message}>
+                </input>
+                <button onClick={sendMessageHandler}><IoIosSend /></button>
+            </div>
         </div>
     );
 };
