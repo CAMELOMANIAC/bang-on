4ae5254a6111ef69c3ca622639f8eda8f0f533d9
yugyusang <yugyusang12@gmail.com>
2024-08-05 23:10:48 +0900

    서비스워커 및 서비스워커등록 그리고 알림 권한 메세지출력

--- src/index.js
+++ src/index.js
@@ -22,7 +22,36 @@ const router = createBrowserRouter([
 		errorElement: <Error />
 	}
 ]);
+//tanstack/react-query의 QueryClient 인스턴스 생성
 const queryClient = new QueryClient();
+
+// 서비스 워커 등록
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("/service-worker.js")
+			.then((registration) => {
+				console.log("ServiceWorker registration successful with scope: ", registration.scope);
+			})
+			.catch((error) => {
+				console.log("ServiceWorker registration failed: ", error);
+			});
+	});
+}
+// Notification 권한 요청
+Notification.requestPermission().then((permission) => {
+	if (permission === "granted") {
+		new Notification("bang-on", {
+			body: "알림 권한이 허용되었습니다.",
+			icon: "/logo192.png"
+		});
+	} else {
+		window.alert(
+			"방송알림을 받으려면 알림권한이 필요합니다\n앱설정에서 사용중인 브라우저의 알림과\n브라우저 설정에서 이 사이트에대한 알림 허용을 혀용해주세요"
+		);
+	}
+});
+
 root.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
