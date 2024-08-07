dc212be9f25ee421f317e44d2f19d912ab724a4b
yugyusang <yugyusang12@gmail.com>
2024-08-07 21:49:24 +0900

    커서가 렌더링되면 useCursorRefArray에 ref를 저장하도록 기능 추가

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef } from "react";
 import { FaMousePointer } from "react-icons/fa";
 import { interpolateData } from "../utils/function/common";
+import { useCursorRefArray } from "../utils/store/store";
 
 const Cursor = ({ data, name, className }) => {
     const ref = useRef(null);
+    const { cursorRefArray, addCursorRef, removeCursorRef } = useCursorRefArray();
 
     useEffect(() => {
         const interpolateCoordinateData = interpolateData(data, 60)//끈켜보이지 않도록 위치정보를 60개로 보간
@@ -22,17 +24,29 @@ const Cursor = ({ data, name, className }) => {
             }
             requestAnimationFrame(animate);
         }
+
         requestAnimationFrame(animate);
         return () => {
             cancelAnimationFrame(animate);
         }
-
     }, [data]);
+
+    //커서 ref를 전역에서 관리하기위해 스토어에 추가합니다.
+    useEffect(() => {
+        const currentRef = ref.current;
+        if (!cursorRefArray.some(ref => ref === currentRef)) {
+            addCursorRef(currentRef);
+        }
+        return () => {
+            removeCursorRef(currentRef);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [addCursorRef, removeCursorRef]);
 
     return (
         <div ref={ref} className={className}>
             <FaMousePointer />
-            {name}
+            <p className="cursor_name">{name}</p>
         </div>
     );
 };
